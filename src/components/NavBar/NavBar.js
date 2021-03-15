f5abb7a696b62882c82d019d7af5303e0c2bd538
Marcin-Migdal <mortimor132@gmail.com>
2021-03-15 21:06:45 +0100

    first version of navBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './NavBar.module.css'
 
 export default function NavBar() {
+  const [toggle, toggleNav] = useState(false);
+
   return (
-    <div>
-      empty
-    </div>
+    <div className={styles.navBarContainer}>
+      <ul className={styles.list}>
+        <li>
+          <p className={styles.navBarSectionText}>
+            O mnie
+          </p>
+        </li>
+        <li>
+          <p className={styles.navBarSectionText}>
+            Technologie
+          </p>
+        </li>
+        <li>
+          <p className={styles.navBarSectionText}>
+            Projekty
+          </p>
+        </li>
+        <li>
+          <p className={styles.navBarSectionText}>
+            Kontakt
+          </p>
+        </li>
+      </ul>
+      <button className={styles.navIcon} onClick={() => toggleNav(!toggle)}>
+        <span className={styles.line} />
+        <span className={styles.line} />
+        <span className={styles.line} />
+      </button>
+      <div className={toggle ? styles.OverlayOpen : styles.OverlayClosed}>
+        <ul className={styles.OverlayMenu}>
+          <li>
+            <p className={styles.navBarSectionText}>
+              O mnie
+          </p>
+          </li>
+          <li>
+            <p className={styles.navBarSectionText}>
+              Technologie
+          </p>
+          </li>
+          <li>
+            <p className={styles.navBarSectionText}>
+              Projekty
+          </p>
+          </li>
+          <li>
+            <p className={styles.navBarSectionText}>
+              Kontakt
+          </p>
+          </li>
+        </ul>
+      </div>
+    </div >
   )
 }
