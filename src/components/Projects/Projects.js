ac6c947863a53471c56eb704413580a263b087a6
morti132 <marcin.migdal1996@gmail.com>
2021-03-30 12:31:47 +0200

    extracted project into reusable projectComponent

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './Projects.module.css'
 import welcomeImage from '../../resourse/images/projects/munchtrack/desktop/welcomePage.png'
 import extendedGameImage from '../../resourse/images/projects/munchtrack/desktop/extendedGame.png'
@@ -17,47 +17,69 @@ import roomsImageMobile from '../../resourse/images/projects/munchtrack/mobile/r
 import searchBarImageMobile from '../../resourse/images/projects/munchtrack/mobile/searchBar.png'
 import searchResultImageMobile from '../../resourse/images/projects/munchtrack/mobile/searchResult.png'
 import settingsImageMobile from '../../resourse/images/projects/munchtrack/mobile/settings.png'
+import ProjectComponent from '../ProjectComponent/ProjectComponent'
 
 export default function Projects() {
-  const [imageIndex, setImageIndex] = useState(0);
-  const [mobileImage, setMobileImage] = useState(false);
-
-  const projectImageArray = mobileImage ?
-    [
-      welcomeImageMobile,
-      roomsImageMobile,
-      searchBarImageMobile,
-      searchResultImageMobile,
-      roomImageMobile,
-      settingsImageMobile,
-      gameImageMobile,
-      extendedGameImageMobile
-    ] :
-    [
-      welcomeImage,
-      roomsImage,
-      searchBarImage,
-      searchResultImage,
-      roomImage,
-      settingsImage,
-      gameImage,
-      extendedGameImage
-    ]
-
-  const prevImage = () => {
-    if (imageIndex - 1 >= 0) {
-      setImageIndex(imageIndex - 1)
-    }
+  const munchtrackContentObject = {
+    projectName: 'Munchtrack',
+    description: 'Serwis Munchtrack został stworzony z myślą aby umożliwić łatwiejsze śledzenie rozgrywki w grze karcianej munchkin,' +
+      'serwis umożliwia założenie konta użytkownika oraz stworzenie pokoi do których mogą dołączać inni użytkownicy,' +
+      'po dołączeniu do pokoju mogą oni zapisywać postęp swojej postaci oraz obserwować postacie swoich przeciwników.',
+    images: {
+      mobile: [
+        welcomeImageMobile,
+        roomsImageMobile,
+        searchBarImageMobile,
+        searchResultImageMobile,
+        roomImageMobile,
+        settingsImageMobile,
+        gameImageMobile,
+        extendedGameImageMobile
+      ],
+      desktop: [
+        welcomeImage,
+        roomsImage,
+        searchBarImage,
+        searchResultImage,
+        roomImage,
+        settingsImage,
+        gameImage,
+        extendedGameImage
+      ]
+    },
+    techStack: {
+      frontEnd: ['React', 'React-Hooks', 'React-Redux', 'React-Router', 'Material-Ui', 'i18next', 'React-Icons', 'React-Perfect-Scrollbar'],
+      backEnd: ['Java', 'Spring-Boot', 'MySql']
+    },
+    githubUrl: "https://github.com/Marcin-Migdal/Munchtrack-client"
   }
 
-  const nextImage = () => {
-    if (imageIndex + 1 < projectImageArray.length) {
-      setImageIndex(imageIndex + 1)
-    }
+  const portfolioContentObject = {
+    projectName: 'Portfolio',
+    description: 'Moje portfolio',
+    images: {
+      mobile: [welcomeImageMobile],
+      desktop: [welcomeImage]
+    },
+    techStack: {
+      frontEnd: ['React', 'React-Hooks', 'i18next'],
+      backEnd: false
+    },
+    githubUrl: "https://github.com/Marcin-Migdal/Portfolio"
   }
 
-  const setImageType = (imageType) => {
-    setMobileImage(imageType)
+  const lotteryContentObject = {
+    projectName: 'EthoLottery',
+    description: 'loteria cyca',
+    images: {
+      mobile: [welcomeImageMobile],
+      desktop: [welcomeImage]
+    },
+    techStack: {
+      frontEnd: ['React', 'React-Hooks', 'web3'],
+      backEnd: ['Solidity']
+    },
+    githubUrl: "https://github.com/Marcin-Migdal/Portfolio"
   }
 
   return (
@@ -65,92 +87,11 @@ export default function Projects() {
       <div className="titleContainer">
         <p className="title">Projekty</p>
       </div>
-      <div className={styles.projectContainer}>
-        <p className={styles.name}>Munchtrack</p>
-        <div className={styles.switchButtonsContainer}>
-          <button
-            className={!mobileImage ? `${styles.switchButton} ${styles.switchButtonActive}` : styles.switchButton}
-            onClick={() => setImageType(false)}>
-            Desktop
-          </button >
-          <button
-            className={mobileImage ? `${styles.switchButton} ${styles.switchButtonActive}` : styles.switchButton}
-            onClick={() => setImageType(true)}>
-            Mobile
-          </button>
-        </div>
-        <div className={mobileImage ? styles.mobileImageContentContainer : styles.desktopImageContentContainer}>
-          <div className={styles.imageContainer}>
-            <button className={styles.imageButton} onClick={prevImage}>
-              {'<'}
-            </button>
-            <div className={styles.upperRightHighlight} />
-            <img className={mobileImage ? styles.mobileImage : styles.image} src={projectImageArray[imageIndex]} />
-            <div className={mobileImage ? styles.lowerRightHighlight : styles.lowerLeftHighlight} />
-            <button className={styles.imageButton} onClick={nextImage}>
-              {'>'}
-            </button>
-          </div>
-          <div className={mobileImage ? styles.sideDescriptionContainer : styles.bottomDescriptionContainer}>
-            <div className={styles.nameContainer}>
-              <hr className={styles.nameLine} />
-              <p className={styles.projectSectionTitle}>Opis</p>
-              <hr className={styles.nameLine} />
-            </div>
-            <span className={styles.projectDescription}>
-              Serwis Munchtrack został stworzony z myślą aby umożliwić łatwiejsze śledzenie rozgrywki w grze karcianej munchkin,
-              serwis umożliwia założenie konta użytkownika oraz stworzenie pokoi do których mogą dołączać inni użytkownicy,
-              po dołączeniu do pokoju mogą oni zapisywać postęp swojej postaci oraz obserwować postacie swoich przeciwników.
-            </span>
-          </div>
-        </div>
-        <div className={styles.nameContainer}>
-          <hr className={styles.nameLine} />
-          <p className={styles.projectSectionTitleShort}>Technologie i biblioteki</p>
-          <hr className={styles.nameLine} />
-        </div>
-        <div className={styles.techStackContainer}>
-          <ul className={styles.techStackList}>
-            <p className={styles.projectSectionSubTitle}>Front-End:</p>
-            <li className={styles.listText}>
-              React
-            </li>
-            <li className={styles.listText}>
-              React-Redux
-            </li>
-            <li className={styles.listText}>
-              React-Router
-            </li>
-            <li className={styles.listText}>
-              Material-Ui
-            </li>
-            <li className={styles.listText}>
-              i18next
-            </li>
-            <li className={styles.listText}>
-              React-Icons
-            </li>
-            <li className={styles.listText}>
-              React-Perfect-Scrollbar
-            </li>
-          </ul>
-          <ul className={styles.techStackList}>
-            <p className={styles.projectSectionSubTitle}>Back-End:</p>
-            <li className={styles.listText}>
-              Java
-            </li>
-            <li className={styles.listText}>
-              Spring-Boot
-            </li>
-            <li className={styles.listText}>
-              MySql
-            </li>
-          </ul>
-        </div>
-        <a className="button" href="https://github.com/Marcin-Migdal/Munchtrack-client">GitHub</a>
-      </div>
-
+      <ProjectComponent projectContentObject={munchtrackContentObject} />
       <hr className={styles.horizontalHr} />
+      <ProjectComponent projectContentObject={portfolioContentObject} />
+      <hr className={styles.horizontalHr} />
+      <ProjectComponent projectContentObject={lotteryContentObject} />
     </div>
   )
 }
